f2f752e09cfd7693365cf0d37613ccbb87b53f62
ethicnology <ethicnology@pm.me>
2020-01-30 11:03:31 +0100

    fuck date in scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import java.util.Date
+import java.text.SimpleDateFormat
 
 case class Cat(name : String, race : String, age: Int)
 case class Person(firstName: String, lastName: String, salary: Int, numberOfChildren : Int, age : Int)
@@ -7,10 +8,14 @@ case class Film(mainActors: Seq[String], dateOfRelease: Date)
 case class Actor(name: String, filmsPlayed: Seq[String])
 
 object Main extends App {
+  val format = new SimpleDateFormat("dd/MM/yyyy") 
+  var date = format.parse("22/05/1970")  
 
-  val Judy = Cat("Judy", "MainCoon", 6)
-  val John = Person("John", "Doe", 0, 0, 20)
-  val R8 = Car("Audi", "Germany", 250, 100)
-  val Al = Actor("Pacino", Seq("Scarface", "Godfather", "Devil advocate"))
-  println(Al)
+  val judy = Cat("Judy", "MainCoon", 6)
+  val john = Person("John", "Doe", 0, 0, 20)
+  val r8 = Car("Audi", "Germany", 250, 100)
+  val devileAdvocate = Film(Seq("Al Pacino", "Keanu Reeves"), date)
+  val al = Actor("Pacino", Seq("Scarface", "Godfather", "Devil advocate"))
+  
+  println(devileAdvocate)
 }
