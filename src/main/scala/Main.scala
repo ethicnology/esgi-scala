331e215a92217354ab563b5475ab0162520c2b63
ethicnology <ethicnology@pm.me>
2020-01-30 10:59:38 +0100

    case class OK

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
+import java.util.Date
+
+case class Cat(name : String, race : String, age: Int)
+case class Person(firstName: String, lastName: String, salary: Int, numberOfChildren : Int, age : Int)
+case class Car(brand: String, countryOfBirth: String, maxSpeed: Int, speeds: Int)
+case class Film(mainActors: Seq[String], dateOfRelease: Date)
+case class Actor(name: String, filmsPlayed: Seq[String])
+
 object Main extends App {
-  println("Hello, World!")
+
+  val Judy = Cat("Judy", "MainCoon", 6)
+  val John = Person("John", "Doe", 0, 0, 20)
+  val R8 = Car("Audi", "Germany", 250, 100)
+  val Al = Actor("Pacino", Seq("Scarface", "Godfather", "Devil advocate"))
+  println(Al)
 }
