0090f03e272e8df05245a5a2fd0ea4841a0a4e9e
ethicnology <ethicnology@pm.me>
2020-02-01 10:44:08 +0100

    Try to cast integer before building the class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import java.util.Date
 import java.text.SimpleDateFormat
 import scala.io.Source
+import scala.util.Try
 
 case class Cat(name : String, race : String, age: Int)
 case class Person(firstName: String, lastName: String, salary: Int, numberOfChildren : Int, age : Int)
@@ -19,32 +20,33 @@ object Main extends App {
   val devileAdvocate = Film(Seq("Al Pacino", "Keanu Reeves"), date)
   val al = Actor("Pacino", Seq("Scarface", "Godfather", "Devil advocate"))
 
-var cpt : Int = 1
-val bufferedSource = Source.fromFile(dataset)
-for (line <- Source.fromFile(dataset).getLines) {
-    val column = line.split(",").map(_.trim)
-    if( column.length == 2){
-      val firstSeq = column(0).split(";").map(_.trim)
-      val secondSeq = column(1).split(";").map(_.trim)
-      if (firstSeq.length == 2){
-        //val film = Film(Seq(firstSeq(0), firstSeq(1)), date)
-      } else if (secondSeq.length == 3){
-        val actor = Actor(column(0), Seq(secondSeq(0),secondSeq(1),secondSeq(2)))
-        println("line : " + cpt + " " + actor)
+  def tryToInt( s: String ) = Try(s.toInt).toOption
+
+
+  var cpt : Int = 1
+  val bufferedSource = Source.fromFile(dataset)
+  for (line <- Source.fromFile(dataset).getLines) {
+      val column = line.split(",").map(_.trim)
+      if( column.length == 2){
+        val firstSeq = column(0).split(";").map(_.trim)
+        val secondSeq = column(1).split(";").map(_.trim)
+        if (firstSeq.length == 2){
+          //val film = Film(Seq(firstSeq(0), firstSeq(1)), date)
+        } else if (secondSeq.length == 3){
+          val actor = Actor(column(0), Seq(secondSeq(0),secondSeq(1),secondSeq(2)))
+          println("line : " + cpt + " " + actor)
+        }
+      } else if(column.length == 3 && tryToInt(column(2)) != None){
+        val cat = Cat(column(0), column(1), column(2).toInt)
+        println("line : " + cpt + " " + cat)
+      } else if(column.length == 4 && tryToInt(column(2)) != None && tryToInt(column(3)) != None){
+        val car = Car(column(0), column(1), column(2).toInt, column(3).toInt)
+        println("line : " + cpt + " " + car)
+      } else if(column.length == 5 && tryToInt(column(2)) != None && tryToInt(column(3)) != None && tryToInt(column(4)) != None){
+        val person = Person(column(0), column(1), column(2).toInt, column(3).toInt, column(4).toInt)
+        println("line : " + cpt + " " + person)
       }
-    } else if(column.length == 3){
-      val cat = Cat(column(0), column(1), column(2).toInt)
-      println("line : " + cpt + " " + cat)
-    } else if(column.length == 4){
-      val car = Car(column(0), column(1), column(2).toInt, column(3).toInt)
-      println("line : " + cpt + " " + car)
-    } else if(column.length == 5){
-      val person = Person(column(0), column(1), column(2).toInt, column(3).toInt, column(4).toInt)
-      println("line : " + cpt + " " + person)
-    }
-    cpt = cpt + 1
-}
-bufferedSource.close
-
-
+      cpt = cpt + 1
+  }
+  bufferedSource.close
 }
